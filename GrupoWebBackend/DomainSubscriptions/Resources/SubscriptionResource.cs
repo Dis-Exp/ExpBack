cf445fb794a26c1dfcadae6502acfe81f6674625
Leonardo Molina <dev@xempre.com>
2022-09-10 22:49:51 -0500

    Initial implementation of DomainSubscriptions.

---
+++ GrupoWebBackend/DomainSubscriptions/Resources/SubscriptionResource.cs
@@ -0,0 +1,14 @@
+﻿namespace GrupoWebBackend.DomainSubscriptions.Resources
+{
+    public class SubscriptionResource
+    {
+        public int Id { get; set; }
+        public int NumPosts { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool Expired { get; set; }
+
+        // Relationships
+        public int UserId { get; set; }
+    }
+}
