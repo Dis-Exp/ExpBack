bd941154381d698c16cf18348482f45c7871836f
Leonardo Molina <dev@xempre.com>
2022-09-13 22:22:26 -0500

    Remove fowarded headers.

--- GrupoWebBackend/Startup.cs
+++ GrupoWebBackend/Startup.cs
@@ -106,13 +106,12 @@ namespace GrupoWebBackend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseForwardedHeaders(new ForwardedHeadersOptions
-            {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-            });
+            //app.UseForwardedHeaders(new ForwardedHeadersOptions
+            //{
+            //    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            //});
 
             // For this course purpose we allow Swagger in release mode.
-            app.UseForwardedHeaders();
             app.UseDeveloperExceptionPage();
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TimExp Backend v1"));
