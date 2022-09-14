54f91b224c949356600e4578d36e7e0209e5fcf2
Leonardo Molina <dev@xempre.com>
2022-09-13 22:34:02 -0500

    Disable HTTPS redirection.

--- GrupoWebBackend/Startup.cs
+++ GrupoWebBackend/Startup.cs
@@ -125,7 +125,7 @@ namespace GrupoWebBackend
 
             app.UseMiddleware<JwtMiddleware>();
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
