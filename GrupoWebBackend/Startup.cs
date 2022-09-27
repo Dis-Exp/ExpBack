b7b54c19a76ba74f646b5385178ad23da3d93b88
Ricardo Anderson Yalico Roncal <u201920140@upc.edu.pe>
2022-09-26 21:55:33 -0500

    use inMemory db

--- GrupoWebBackend/Startup.cs
+++ GrupoWebBackend/Startup.cs
@@ -70,8 +70,8 @@ namespace GrupoWebBackend
             services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
             services.AddDbContext<AppDbContext>(options =>
             {
-                options.UseMySQL("server=localhost; user=timexp; database=timexp; password=XempreDB; port=3306");
-                //options.UseInMemoryDatabase("GrupoWebBackend-api-in-memory");
+                //options.UseInMemoryDatabase("server=localhost; user=timexp; database=timexp; password=XempreDB; port=3306");
+                options.UseInMemoryDatabase("GrupoWebBackend-api-in-memory");
             });
             services.AddSwaggerGen(c =>
             {
